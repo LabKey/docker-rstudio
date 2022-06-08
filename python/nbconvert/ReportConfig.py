f17ef4d42bc296f832d6bdd1013e534a43e287d8
Matthew Bellew <matthewb@labkey.com>
2022-06-07 18:01:58 -0700

    get_source_data()

--- python/nbconvert/ReportConfig.py
+++ python/nbconvert/ReportConfig.py
@@ -5,7 +5,7 @@ from urllib.parse import urlparse
 
 
 DEFAULT_REPORT_CONFIG_FILE = 'report_config.json'
-USER_ENV = "LABKEY_EMAIL"       # note USER is common UNIX env, don't use
+USER_EMAIL_ENV = "LABKEY_EMAIL"       # note USER is common UNIX env, don't use
 APIKEY_ENV = "LABKEY_API_KEY"    
 
 
@@ -16,12 +16,18 @@ APIKEY_ENV = "LABKEY_API_KEY"
 # camelCase naming convention.
 
 config_example = { 
-        'baseUrl' : "http://localhost:8080/labkey/",
-        'containerPath' : "/home", 
-        'apiKey' : "{{APIKEY}}",
-        'user' : "user1@test.com",
-        'parameters' : { 'xmin':0, 'xmax':100, 'type':"sample" }
+        "baseUrl" : "http://localhost:8080/labkey/",
+        "containerPath" : "/home", 
+        "apiKey" : "{{APIKEY}}",        # optionally passed in via environment
+        "email" : "user1@test.com",     # optionally passed in via environment
+        "parameters" : { 'xmin':0, 'xmax':100, 'type':"sample" },
+        "sourceQuery": {
+            "columns":[["X"],["ModifiedBy", "DisplayName"]], 
+            "requiredVersion":17.1, 
+            "queryName":"asdf", 
+            "schemaName":"lists"
         }
+    }
 #
 
 
@@ -32,9 +38,9 @@ class ReportConfig:
 
     def __init__(self, config=None, config_file=None):
         self.api_key = None
-        self.user = None
+        self.email = None
         self.api = None
-        self.json = {}
+        self.config = {}
        
         if config is None:
             if config_file is None and os.path.isfile(DEFAULT_REPORT_CONFIG_FILE):
@@ -51,10 +57,10 @@ class ReportConfig:
         if self.config.get("apiKey"):
             self.api_key = self.config.get("apiKey")
         
-        if os.getenv(USER_ENV):
-            self.user = os.getenv(USER_ENV)
-        if self.config.get("user"):
-            self.user = self.config.get("user")
+        if os.getenv(USER_EMAIL_ENV):
+            self.email = os.getenv(USER_EMAIL_ENV)
+        if self.config.get("email"):
+            self.email = self.config.get("email")
 
         if self.config.get('baseUrl'):
             url = urlparse(self.config.get('baseUrl'))
@@ -68,14 +74,51 @@ class ReportConfig:
         if not self.config['domain'] or self.config['containerPath'] is None or self.config['contextPath'] is None or self.config['useSsl'] is None:
             raise Exception("Could not construct LabKey server URL")
 
-        self.api = APIWrapper(self.config['domain'], self.config['containerPath'], self.config['contextPath'], self.config['useSsl'])
+        self.api = APIWrapper(self.config['domain'], self.config['containerPath'], self.config['contextPath'], self.config['useSsl'],
+                api_key=self.api_key)
         return
+
+
+    def get_source_data(self):
+        if not 'sourceQuery' in self.config:
+            return None
+        q = self.config['sourceQuery']
+        columns = "*"
+        if 'columns' in q:
+            columns = q['columns']
+        sort = ""
+        if 'sort' in q:
+            sort = q['sort']
+        query_filters = []
+        if 'filterArray' in q:
+            array = q['filterArray']
+            for i in range(0,len(array)):
+                f = array[i]
+                query_filters.add(QueryFilter(f[0],f[1],f[2]))
+        parameters = {}
+        if 'parameters' in q:
+            parameters = q['parameters']
+        containerFilter = None
+        if 'containerFilter' in q:
+            containerFilter = q['containerFilter']
+
+        api = self.get_api_wrapper()
+        return api.query.select_rows(q['schemaName'], q['queryName'],
+                #containerFilter=containerFilter,
+                max_rows=-1,
+                offset=0,
+                columns=columns,
+                sort=sort,
+                filter_array=query_filters,
+                parameters=parameters,
+                required_version=q['requiredVersion']
+                )
 
 
     def __str__(self):
         j = dict(self.config)
         j['apiKey'] = self.api_key 
-        j['user'] = self.user
+        j['email'] = self.email
         return str(j)
 
 
@@ -87,3 +130,7 @@ class ReportConfig:
         return
 
 
+#
+report = ReportConfig(config=config_example)
+data = report.get_source_data()
+print(data)
